5c9fb62556dfca8682e42ebce79c2c0717c17739
Crisano97 <97keb6@gmail.com>
2022-09-07 16:22:20 +0200

    main structure added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layout.main')
 
 @section('main-content')
-    io sono il main
+    <div class="jumbotron"></div>
+    <div class="wrapper position-relative">
+        <h4 class="current-series-comics">CURRENT SERIES</h4>
+        <div class="cards d-flex justify-content-between flex-wrap">
+            
+        </div>
+        <div class="btn-container">
+            <button class="btn-more">LOAD MORE</button>    
+        </div>     
+    </div>
 @endsection
