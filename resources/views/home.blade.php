963258aa9e081d27f51a1b636dced9924f3ff517
Crisano97 <97keb6@gmail.com>
2022-09-07 16:34:27 +0200

    page full structure without main contents

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,28 @@
     <div class="wrapper position-relative">
         <h4 class="current-series-comics">CURRENT SERIES</h4>
         <div class="cards d-flex justify-content-between flex-wrap">
-            
+            <div class="comic-card">
+                <div class="image-container">
+                    <img src="" alt="comicType">
+                </div>
+                <h6 class="text-uppercase mt-4">
+                    
+                </h6>
+            </div>
         </div>
         <div class="btn-container">
             <button class="btn-more">LOAD MORE</button>    
         </div>     
+    </div>
+    <div class="wrapper">
+        <div class="main-nav">
+                <div class="link-wrapper d-flex justify-content-between p-4 align-items-center">
+                    <div class="link">
+                        <img class="link-img" src="" alt="link.text">
+                        <span class="text-uppercase ms-2">  </span>
+                    </div>
+                    
+                </div>
+        </div>
     </div>
 @endsection
--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,38 @@
 <footer>
-    io sono il footer
+<div class="footer-top d-flex justify-content-around">
+            <div class="wrapper">
+                <nav>
+                <div class="list-wrapper d-flex flex-wrap justify-content-between w-50 pt-4">
+                    <div class="nav-lists">
+                        <h5 class="text-white fw-bold text-uppercase">
+                            
+                        </h5>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    comics
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            </div>
+        </div>
+        <div class="py-4 footer-bottom">
+            <div class="container-bottom  d-flex justify-content-between">
+                <button class="sign-up">SIGN-UP NOW!</button>
+                <div class="follow d-flex align-items-center justify-content-end">
+                    <span>FOLLOW US</span>
+                    <div class="img-wrapper d-flex justify-content-around w-50 ms-3">
+                        <img src="../../assets/images/footer-facebook.png" alt="">
+                        <img src="../../assets/images/footer-twitter.png" alt="">
+                        <img src="../../assets/images/footer-youtube.png" alt="">
+                        <img src="../../assets/images/footer-pinterest.png" alt="">
+                        <img src="../../assets/images/footer-periscope.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+  </footer>
 </footer>
